5f21525a7822c7feea07394a4a8c21d3fd80048c
andersonduarte01 <dev.brasil.pb@gmail.com>
2022-08-03 14:37:23 -0300

    botao

--- sistema/settings.py
+++ sistema/settings.py
@@ -78,14 +78,21 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'system',
+#         'USER': 'root',
+#         'PASSWORD': 'sosa1808',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'system',
-        'USER': 'root',
-        'PASSWORD': 'sosa1808',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
