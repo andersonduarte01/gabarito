bdf4a14e5f6e081ca08a2aad8474254bf0804efa
andersonduarte01 <dev.brasil.pb@gmail.com>
2023-02-23 21:22:50 -0300

    web

--- apps/arquivos/models.py
+++ apps/arquivos/models.py
@@ -56,22 +56,14 @@ def update_imagem(sender, instance, **kwargs):
         if not instance.pdf_miniatura:
             #save_dir = r'C:\Users\Anderson\Desktop\projeto\SME\gabarito\media\miniaturas'
             save_dir = r'/home/anderson/projeto/gabarito/media/miniaturas'
-            print('Passo 01')
-            arquivo = instance.pdf.path
-            print('Passo 02')
+            arquivo = instance.pdf.path.encode('UTF-8')
             images_from_path = convert_from_path(arquivo, output_folder=save_dir, fmt='.jpg',
-                                                 first_page=0, last_page=1, size=(200, 280))
-            print('Passo 03')
+                                                 first_page=0, last_page=1, size=(400, 560))
             blob = open(images_from_path[0].filename, 'rb')
-            print('Passo 04')
             fi = blob.read()
-            print('Passo 05')
             blob.close()
-            print('Passo 06')
             instance.pdf_miniatura.save(f'{instance.descricao}.jpeg', ContentFile(fi), save=False)
-            print('Passo 07')
             instance.save()
-            print('Passo 08')
     except:
         print('Error de formato de documento')
 
@@ -82,7 +74,7 @@ def update_imagem(sender, instance, **kwargs):
         if not instance.pdf_miniatura:
             #save_dir = r'C:\Users\Anderson\Desktop\projeto\SME\gabarito\media\miniaturas'
             save_dir = r'/home/anderson/projeto/gabarito/media/miniaturas'
-            livro = instance.pdf.path
+            livro = instance.pdf.path.encode('UTF-8')
             images_from_path = convert_from_path(livro, output_folder=save_dir, fmt='.jpg',
                                                  first_page=0, last_page=1, size=(230, 390))
             blob = open(images_from_path[0].filename, 'rb')
