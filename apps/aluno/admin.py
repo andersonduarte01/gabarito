7321ac8fba48d9dc3338f4b3ad2e576d8891ff5e
andersonduarte01 <dev.brasil.pb@gmail.com>
2023-05-17 23:14:05 -0300

    admin

--- apps/aluno/admin.py
+++ apps/aluno/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from apps.aluno.models import Aluno
+from ..aluno.models import Aluno
 
 admin.site.register(Aluno)
--- apps/sala/admin.py
+++ apps/sala/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from ..sala.models import Ano, Sala
 
-admin.site.register(Sala)
+
+class SalaAdmin(admin.ModelAdmin):
+    model = Sala
+    list_display = ['descricao', 'escola', 'ano']
+
+
+admin.site.register(Sala, SalaAdmin)
 admin.site.register(Ano)
