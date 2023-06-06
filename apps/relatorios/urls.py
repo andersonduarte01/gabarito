80e5a36cf712330633bac42749800350fb9b931b
andersonduarte01 <dev.brasil.pb@gmail.com>
2023-06-06 10:34:09 -0300

    relates1

--- apps/relatorios/urls.py
+++ apps/relatorios/urls.py
@@ -5,7 +5,6 @@ app_name = 'relatorios'
 urlpatterns = [
     path('<int:aluno>/<slug:slug>/<int:avaliacao_aluno>/', views.RelatorioAluno.as_view(), name='relatorio_aluno'),
     path('<slug:slug>/', views.RelatorioEscola.as_view(), name='relatorio_escola'),
-    path('<slug:slug>/<int:avaliacao_id>/<int:pk>/', views.RelatorioSala.as_view(), name='relatorio_sala'),
     path('<slug:slug>/<int:id>/<int:avaliacao_id>/', views.RelatorioAvaliacao.as_view(), name='relatorio_avaliacao'),
 
 ]
