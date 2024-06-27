928537535b19d1deb3ee16cac1d4ba0c3111e76b
andersonduarte01 <dev.brasil.pb@gmail.com>
2024-06-27 12:53:05 -0300

    relatorios

--- apps/frequencia/admin.py
+++ apps/frequencia/admin.py
@@ -6,4 +6,11 @@ admin.site.register(Frequencia)
 admin.site.register(FrequenciaAluno)
 admin.site.register(Registro)
 admin.site.register(Periodo)
-admin.site.register(Relatorio)
+
+
+class RelatorioAdmin(admin.ModelAdmin):
+    list_display = ('periodo', 'aluno', 'professor', 'data_relatorio', 'atualiza_relatorio')
+    search_fields = ('aluno__nome', 'professor__professor_nome', 'periodo__periodo')  # Ajuste os campos conforme a sua necessidade
+
+
+admin.site.register(Relatorio, RelatorioAdmin)
