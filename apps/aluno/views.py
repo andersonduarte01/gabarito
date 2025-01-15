de2bccc1770d589fdff8735f27afced804950849
andersonduarte01 <dev.brasil.pb@gmail.com>
2025-01-15 09:20:50 -0300

    relatori

--- apps/aluno/views.py
+++ apps/aluno/views.py
@@ -199,7 +199,7 @@ def relatorioAluno(request, pk, mes):
                    y=(ponto3[1] - 85), font="Helvetica", font_size=14,
                    color=(0, 0, 0))
 
-    altura = adicionar_linha_paralela(c, ponto3, ponto4, intervalo=118.65)
+    altura1 = adicionar_linha_paralela(c, ponto3, ponto4, intervalo=118.65)
 
     locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
 
@@ -232,7 +232,6 @@ def relatorioAluno(request, pk, mes):
         ('GRID', (0, 0), (-1, -1), 1, (0.8, 0.8, 0.8)),
     ])
 
-    altura_padrao = adicionar_linha_paralela(c, ponto3, ponto4, intervalo=0)
 
     for aluno in alunos:
         alunos_tag = []
@@ -267,12 +266,12 @@ def relatorioAluno(request, pk, mes):
         percorrer -= 25
 
     t = Table(resultado, colWidths=larguras_colunas)
-    largura_tabela, altura_tabela = t.wrapOn(None, largura_disponivel, 500)
+    largura_tabela, altura_tabela = t.wrapOn(None, largura_disponivel, 600)
     t.setStyle(style)
     largura_tabela = 27 * 28.35
     posicao_horizontal_tabela = ponto4[0]
     t.wrapOn(c, largura_tabela, 500)
-    t.drawOn(c, posicao_horizontal_tabela, altura_padrao - 200)
+    t.drawOn(c, 39, altura1 - 85)
 
     c.save()
     return response
